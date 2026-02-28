8d5d4f7c00b92d0ae484cf9670b86288a401d3b0
setthawut <nu9g37@gmail.com>
2026-02-28 10:33:07 +0700

    Initial Commit

---
+++ controllers/campgrounds.js
@@ -0,0 +1,179 @@
+const Appointment = require('../models/Booking');
+const Hospital = require('../models/Campground');
+// @desc    Get all hospitals      
+// @route   GET /api/v1/hospitals
+// @access  Public
+exports.getHospitals = async (req, res, next) => {
+  let query;
+
+  // Copy req.query
+  const regQuery = {...req.query};
+  
+  // Field to exclude
+  const removeFields = ['select', 'sort', 'page', 'limit'];
+
+  // Loop over remove fields and delete them from query
+  removeFields.forEach(param => delete regQuery[param]);
+  console.log(regQuery);
+
+  // Create query string
+  let queryStr = JSON.stringify(regQuery);
+  
+  // Create operator
+  queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, match=>`$${match}`);
+
+  // finding resource
+  query = Hospital.find(JSON.parse(queryStr)).populate('appointments');
+
+  // Select fields
+  if (req.query.select) {
+    const fields = req.query.select.split(',').join(' ');
+    query = query.select(fields);
+  }
+
+  // Sort
+  if (req.query.sort) {
+    const sortBy = req.query.sort.split(',').join(' ');
+    query = query.sort(sortBy);
+  } else {
+    query = query.sort('-createdAt');
+  }
+
+  // Pagination
+  const page = parseInt(req.query.page, 10) || 1;
+  const limit = parseInt(req.query.limit, 10) || 25;
+
+  const startIndex = (page - 1) * limit;
+  const endIndex = page * limit;
+
+  try {
+    const total = await Hospital.countDocuments();
+    query = query.skip(startIndex).limit(limit);
+
+    const hospitals = await query;
+
+    // Pagination result
+    const pagination = {};
+    
+    if (endIndex < total) {
+      pagination.next = {
+        page: page + 1,
+        limit
+      }
+    }
+
+    if (startIndex > 0) {
+      pagination.prev = {
+        page: page - 1,
+        limit
+      }
+    }
+
+    res.status(200).json({
+      success: true,
+      count: hospitals.length,
+      pagination,
+      data: hospitals
+    });
+  } catch (err) {
+    res.status(400).json({
+      success: false
+    });
+  }
+};
+
+// @desc    Get single hospitals      
+// @route   GET /api/v1/hospitals/:id
+// @access  Public
+exports.getHospital = async (req, res, next) => {
+  try {
+    const hospital = await Hospital.findById(req.params.id);
+
+    if (!hospital) {
+      return res.status(400).json({
+        success: false
+     }); 
+    }
+
+    res.status(200).json({
+      success: true,
+      data: hospital
+    });
+  } catch (err) {
+    res.status(404).json({
+      success: false
+    });
+  }
+};
+
+// @desc    Create new hospital      
+// @route   PUSH /api/v1/hospitals
+// @access  Private
+exports.createHospital = async (req, res, next) => {
+  // create new Hospital and add to database 
+  const hospital = await Hospital.create(req.body);
+  
+  res.status(201).json({
+    success: true, 
+    data: hospital
+  });
+};
+
+// @desc    Update hospital      
+// @route   PUT /api/v1/hospitals/:id
+// @access  Private
+exports.updateHospital = async (req, res, next) => {
+  try {
+    const hospital = await Hospital.findByIdAndUpdate(req.params.id, req.body,
+      {
+        new: true,
+        runValidators: true
+      }
+    );
+
+    if (!hospital) {
+      return res.status(400).json({
+        success: false
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      data: hospital
+    });
+  } catch (err) {
+    res.status(400).json({
+      success: false
+    });
+  }
+};
+
+// @desc    Delete hospital      
+// @route   DELETE /api/v1/hospitals/:id
+// @access  Private
+exports.deleteHospital = async (req, res, next) => {
+  try {
+    const hospital = await Hospital.findById(req.params.id);
+
+    if (!hospital) {
+      return res.status(404).json({
+        success: false,
+        msg: `Hospital not found with id of ${req.params.id}`
+      });
+    }
+
+    await Appointment.deleteMany({hospital: req.params.id});
+    await Hospital.deleteOne({_id: req.params.id});
+
+    res.status(200).json({
+      success: true, 
+      data: hospital
+    });
+  } catch (err) {
+    // console.log(err);
+
+    res.status(400).json({
+      success:false,
+    });
+  }
+};
