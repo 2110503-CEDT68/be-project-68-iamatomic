3d1c18912136306fa31380927f5e9c83ea6fb027
Pattadon Plubladpho <pattadonplubladpho@gmail.com>
2026-03-02 13:43:10 +0700

    add UML file

--- controllers/auth.js
+++ controllers/auth.js
@@ -94,7 +94,7 @@ exports.login = async (req, res, next) => {
 };
 
 // @desc    Get current login user
-// @route   POST /api/v1/auth/me
+// @route   GET /api/v1/auth/me
 // @access  Private
 exports.getMe = async (req, res, next) => {
   const user = await User.findById(req.user.id);
